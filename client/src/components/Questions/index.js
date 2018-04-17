8ec516a19663ae583fe83d763f324b6ab4d0e767
Edgardo Mendez-Soto <gwenchy15@gmail.com>
2018-04-17 17:56:55 -0400

    update

--- client/src/components/Questions/index.js
+++ client/src/components/Questions/index.js
@@ -9,10 +9,21 @@ const Questions = (props) => {
 	console.log(props)
 	return (
 		<div>
+			<form>
 			<h4>{props.questionAsked}</h4>
-			<ul>
-				{props.answers.map((answer, index) => <li key={index}>{answer}</li>)}
-			</ul>
+				<div className="form-check">
+					{props.answers.map((answer, index) =>{
+						return (
+							<div key={index}>
+								<input className="form-check-input" type="radio" name="exampleRadios" value={answer} />
+								<label className="form-check-label" htmlFor="exampleRadios">
+									{answer}
+								</label>
+							</div>
+						);
+					})}
+				</div>
+			</form>
 		</div>
 	);
 }
